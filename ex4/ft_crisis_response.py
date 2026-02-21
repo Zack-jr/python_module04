38d28f56b1b810005e4f961994672fafa67fc7c0
Zakaria Labib-Girard <girard.zack04@gmail.com>
2026-02-21 17:29:09 +0100

    flake8 remaining

--- ex4/ft_crisis_response.py
+++ ex4/ft_crisis_response.py
@@ -1,31 +1,43 @@
+def handle_crisis(file_name, crisis_type):
+
+    if (crisis_type == "archive_not_found"):
+        try:
+            print(f"CRISIS ALERT: Attempting access to '{file_name}'...")
+            with open(file_name, "r") as f:
+                f.read()
+        except FileNotFoundError:
+            print("RESPONSE: Archive not found in storage matrix")
+        finally:
+            print("STATUS: Crisis handled, system stable\n")
+
+    if (crisis_type == "denied_access"):
+        try:
+            print(f"CRISIS ALERT: Attempting access to '{file_name}'...")
+            with open(file_name, "r") as f:
+                f.read()
+        except PermissionError:
+            print("RESPONSE: Security protocols deny access")
+        finally:
+            print("STATUS: Crisis handled, security maintained\n")
+    
+    if (crisis_type == "routine_access"):
+        try:
+            print(f"ROUTINE ACCESS: Attempting access to '{file_name}'...")
+            with open(file_name, "r") as f:
+                text = f.read()
+            print(f"SUCCESS: Archive recovered - ``{text}''")
+        except (FileNotFoundError, PermissionError):
+            print("Error found.")
+        finally:
+            print("STATUS: Normal operations resumed\n")
+
+
 def crisis_response():
     print("=== CYBER ARCHIVES - CRISIS RESPONSE SYSTEM ===\n")
-
-    try:
-        print("CRISIS ALERT: Attempting access to 'lost_archive.txt'...")
-        with open("lost_archive.txt", "r") as f:
-            f.read()
-    except FileNotFoundError:
-        print("RESPONSE:  Archive not found in  storage matrix")
-    finally:
-        print("STATUS: Crisis handled, system stable")
-
-    try:
-        print("CRISIS ALERT: Attempting access to 'classified_vault.txt'...")
-        with open("classified_vault.txt", "w") as f:
-            f.read()
-    except PermissionError:
-        print("Security protocols deny access")
-    finally:
-        print("STATUS: Crisis handled, security maintained")
-    
-    try:
-        print("ROUTINE ACCESS: Attempting access to 'standard_archive.txt'...")
-        with open("standard_archive.txt", "r") as f:
-            text = f.read()
-        print(f"SUCCESS: Archive recovered - ``{text}''")
-    except FileNotFoundError, PermissionError:
-        
+    handle_crisis("lost_archive.txt", "archive_not_found")
+    handle_crisis("classified_vault.txt", "denied_access")
+    handle_crisis("standard_archive.txt", "routine_access")
+    print("All crisis scenarios handled successfully. Archives secure.")
 
 if __name__ == '__main__':
     crisis_response()
